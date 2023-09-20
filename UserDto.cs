bf26d226f115e5841b687566f709b5e774389056
godwin-nj <amadigodwin7@gmail.com>
2023-09-20 19:10:13 +0100

    Add project files.

---
+++ UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace JWTTUT
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
