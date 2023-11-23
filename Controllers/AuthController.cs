361eda4576f067e7fbc880a0d834050d5cd230c5
godwin-nj <amadigodwin7@gmail.com>
2023-11-23 14:29:23 +0100

    added authentication and authorization to soln

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -50,7 +50,8 @@ namespace JWTTUT.Controllers
         {
             List<Claim> cliams = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.Username)
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, "Admin")
             };
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
